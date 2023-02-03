234918c0a8d5cf86fd7de8a2b9cf9f9a7e3ab1e4
Gerson Santos <62805855+GersonESS@users.noreply.github.com>
2023-02-03 14:33:13 -0300

    r

--- models/Client.php
+++ models/Client.php
@@ -8,7 +8,7 @@
 
         function __construct(){
             parent::__construct();
-            $this->table = "clientes";
+            $this->table = "cliente";
         }
 
         public function getAll(){
