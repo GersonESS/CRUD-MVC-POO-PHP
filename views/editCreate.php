b169069a832b195695949be13eb4c9ed48454732
Gerson Santos <62805855+GersonESS@users.noreply.github.com>
2023-02-05 01:26:50 -0300

    a

--- views/editCreate.php
+++ views/editCreate.php
@@ -11,10 +11,17 @@
     <a class="button btn-back" href="index.php">Volta</a>
     <h1>Clientes config</h1>
     <div class="content">
-        <form action="index.php" method="POST">
+        <form id="form" action="index.php" method="POST">
             <div class="input-box">
                 <label for="name">Nome:</label>
-                <input class="input" type="text" placeholder="Escreva o nome do cliente" value="<?= isset($resultData[0]['nome']) ? $resultData[0]['nome'] : '' ?>" name="nome" required>
+
+                 <!-- oninput="nameValidate()" -->
+
+                 <input class="inputs required" type="text" placeholder="Digite o nome do cliente" value="<?= isset($resultData[0]['nome']) ? $resultData[0]['nome'] : '' ?>" name="nome" required>
+
+                <!-- <input class="inputs required" type="text" placeholder="Digite o nome do cliente" value="<?= isset($resultData[0]['nome']) ? $resultData[0]['nome'] : '' ?>" name="nome" required oninput="nameValidate()"> -->
+                <span class="span-required">Nome deve ter no mínimo 3 caracteres</span>
+
             </div>
             <br><br>
             <div class="input-box">
@@ -33,4 +40,74 @@
         </form>
     </div>
 </body>
+<script>
+    const form   = document.getElementById('form');
+    const campos = document.querySelectorAll('.required');
+    const spans  = document.querySelectorAll('.span-required');
+    const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        nameValidate();
+        emailValidate();
+        mainPasswordValidate();
+        comparePassword();
+    });
+
+    function setError(index){
+        campos[index].style.border = '2px solid #e63636';
+        spans[index].style.display = 'block';
+    }
+
+    function removeError(index){
+        campos[index].style.border = '';
+        spans[index].style.display = 'none';
+    }
+
+    function nameValidate(){
+        if(campos[0].value.length < 3)
+        {
+            setError(0);
+        }
+        else
+        {
+            removeError(0);
+        }
+    }
+
+    function emailValidate(){
+        if(!emailRegex.test(campos[1].value))
+        {
+            setError(1);
+        }
+        else
+        {
+            removeError(1);
+        }
+    }
+
+    function mainPasswordValidate(){
+        if(campos[2].value.length < 8)
+        {
+            setError(2);
+        }
+        else
+        {
+            removeError(2);
+            comparePassword();
+        }
+    }
+
+    function comparePassword(){
+        if(campos[2].value == campos[3].value && campos[3].value.length >= 8)
+        {
+            removeError(3);
+        }
+        else
+        {
+            setError(3);
+        }
+    }
+
+</script>
 </html>
